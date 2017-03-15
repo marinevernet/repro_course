17230aca53e96d33306260d2cfe9c0a8cbf42809
Marine Vernet <marine.vernet@gmail.com>
2017-03-15 11:30:54 -0400

    mo data_not_in_repo in metasarch

--- metasearch_analysis.py
+++ metasearch_analysis.py
@@ -1,3 +1,5 @@
 # coding: utf-8
 # data downlaod
 # get_ipython().system('git clone https://github.com/OpenNeuroLab/metasearch.git')
+Path('data_not_in_repo').mkdir()
+get_ipython().magic('mv metasearch/ data_not_in_repo/')
